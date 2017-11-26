c4340e069ccd1832f00395f2dc0175332ff48819
thatsparrow <jbenjamin001@gmail.com>
2017-11-26 15:05:23 +0530

    type done

--- apps/type/demo/demo.js
+++ apps/type/demo/demo.js
@@ -4,39 +4,40 @@ var corpus1 =
 "Sup?\n\
 \n\
 I'm a naïve simulation of a human typing. I was written in a few lines of CoffeeScript, so I'm not meant to be very accurate. Note that you can easily change my typing accuracy level and speed.\n\
+\n\
 Check this out!";
 
 var corpus2 =
 "Use me like this:\n\
 \n\
-new TuringType(domElement, 'Just some text.');\n\
+new Type(domElement, 'Just some text.');\n\
 \n\
 Or pass some options if you feel like it:\n\
 \n\
-new TuringType(domElement, 'Terrible but fast typist.', { accuracy: 0.3, interval: 20, callback: allDone });\n\
+new Type(domElement, 'Terrible but fast typist.', { accuracy: 0.3, interval: 20, callback: allDone });\n\
 \n\
 Enjoy.";
 
 var cb1 = function(){
     setTimeout(function(){
         el.value = '';
-        new TuringType(el, 'You can set me to be wildly inaccurate.', { accuracy: .5, callback: cb2 });
+        new Type(el, 'You can set me to be wildly inaccurate.', { accuracy: .5, callback: cb2 });
     }, 2000);
 }
 
 var cb2 = function(){
     setTimeout(function(){
         el.value = '';
-        new TuringType(el, 'Or maybe wildly inaccurate and very fast?', { accuracy: .3, interval: 15, callback: cb3 });
+        new Type(el, 'Or maybe wildly inaccurate and very fast?', { accuracy: .3, interval: 15, callback: cb3 });
     }, 2000);
 }
 
 var cb3 = function(){
     setTimeout(function(){
         el.value = '';
-        new TuringType(el, corpus2);
+        new Type(el, corpus2);
     }, 2000);
 }
 
 var el = document.getElementById('test');
-new TuringType(el, corpus1, { callback: cb1 });
+new Type(el, corpus1, { callback: cb1 });

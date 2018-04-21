83aa75fd5fbc2a5a0c353b3db0ab04d5e5c84f20
thatsparrow <jbenjamin001@gmail.com>
2018-04-21 13:21:28 +0530

    few changes

--- assets/js/script.js
+++ assets/js/script.js
@@ -98,10 +98,10 @@ function compileContent() {
      var a = "hc.liamnotorp/worrapstaht".split("").reverse().join("").replace("/", "@");
      var b = {
          NAME: "Jann Benjamin",
-         TITLE: ["Candid Writer", "Agile Developer"],
-         LINKS: ['<a class="item" title="A man of mystery and charm whose charm is exceeded only by his mystery" href="about/">#about</a>','<a class="item" title="An ode to symmetry" href="art/">#art</a>', '<a class="item" title="A summary of my background and skills" href="#">#résumé</a>'],
+         TITLE: ["/Candid Writer /Agile Developer"],
+         LINKS: ['<a class="item" title="" href="about/">About</a>','<a class="item" title="An ode to symmetry" href="art/">Art</a>','<a class="item" title="An ode to symmetry" href="music/">Music</a>','<a class="item" title="A summary of my background and skills" href="#">Résumé</a>'],
          WORK: {
-             position: ["Public Relations Executive", "Content Writer"],
+             position: ["Public Relations Executive"],
              company: '<a title="A multinational media and digital marketing communications company" href="http://www.dentsuaegisnetwork.com/">Dentsu Aegis Network</a>',
              location: "Colombo, Sri Lanka"
          },
@@ -111,25 +111,25 @@ function compileContent() {
              location: "Remote"
          },
          PORTFOLIO: {
-             a: '<a title="Logos, Identities, Prototypes, Typography" href="#">art > branding</a>',
-             b: '<a title="Press Releases, Features, Thought Leadership Articles, AV Scripts, Case Studies, Product Listings, Web Copy, Social Media Copy" href="#">copy > print</a>',
-             c: '<a title="Audio, AVs, CAD, " href="#">etc > assorted</a>',
+             a: '<a title="Logos, Identities, Prototypes, Typography" href="compiling">art > branding</a>',
+             b: '<a title="Press Releases, Features, Thought Leadership Articles, AV Scripts, Case Studies, Product Listings, Web Copy, Social Media Copy" href="compiling">copy > print</a>',
+             c: '<a title="Audio, AVs, CAD" href="compiling">etc > assorted</a>',
                           
          },
 
          PROJECTS: {
-             1: '<a title="A minimal notepad for heavy deadlines" href="#">app > flow</a> : • HTML • CSS • JavaScript',
-             2: '<a title="A UX effect to simulate a human typing" href="#">UI > type</a> : • JavaScript',
-             3: '<a title="A static website crafted with Jekyll, HTML5, CSS3 and Javascript" href="https://github.com/OSIRISagency/OSIRISagency.github.io">web > osiris</a> : • Jekyll',
-             4: '<a title="A to-do list interface" href="apps/list">app > list</a> : • Flask • MySQL • jQuery • PythonAnywhere',
-             5: '<a title="A collaborative code compiler (Python, Java, C, C#, C++, Haskell, Perl, Ruby) built on Heroku and Firebase" href="apps/compile/">apps > compile</a> : • Node.js • Firebase • Heroku',
-             6: '<a title="A collection of CSS3 effects" href="apps/float">UI > float</a> : • jQuery',
-             7: '<a title="A static website crafted with Bootstrap, HTML5, CSS3 and JavaScript" href="https://github.com/353clothing/353clothing.github.io">web > 353</a> : • Bootstrap',
-             8: '<a title="An Android app that finds petrol stations and splits costs" href="#">app > insight</a> : • Android Studio • Google Maps API',
-             9: '<a title="A music interface for helping stay focused" href="apps|kyoto">app > kyoto</a> : • jQuery',
+             1: '<a title="A minimal notepad for heavy deadlines" href="compiling">app > flow</a> : • HTML • CSS • JavaScript',
+             2: '<a title="A UX effect to simulate a human typing" href="compiling">UX > type</a> : • JavaScript',
+             3: '<a title="A static website crafted with Jekyll, HTML5, CSS3 and Javascript" href="compiling">web > osiris</a> : • Jekyll',
+             4: '<a title="A to-do list interface" href="compiling">app > list</a> : • Flask • MySQL • jQuery • PythonAnywhere',
+             5: '<a title="A collaborative code compiler (Python, Java, C, C#, C++, Haskell, Perl, Ruby) built on Heroku and Firebase" href="apps/compile/">app > compile</a> : • Node.js • Firebase • Heroku',
+             6: '<a title="A collection of CSS3 effects" href="compiling">UI > float</a> : • CSS • jQuery',
+             7: '<a title="A static website crafted for a boutique apparel company" href="http://353clothing.com">web > 353</a> : • Bootstrap • Slack',
+             8: '<a title="An Android app that finds petrol stations and splits costs" href="compiling">app > insight</a> : • Android Studio • Google Maps API',
+             9: '<a title="A series of number theory problems hosted on projecteuler.net" href="compiling">code > euler</a> : • Java • Python • Mathematica • Haskell',
          },
         
-         CREDITS: ['<a class="item" href="PGP.md">PGP</a>', '<a class="item" href="REMINDER.md">Reminder</a>','1994-2018 jannbenjam.in&trade;']
+         CREDITS: ['<a class="item" href="PGP.md">PGP</a>', '<a class="item" href="REMINDER.md">Reminder</a>','1994-Present | jannbenjam.in&trade;']
      };
      return "<pre id='" + contentId + "'>" + JSON.convertToHumanReadableString(b, 2, window.innerHeight > window.innerWidth) + "</pre>"
  }

4137206a14852c83dbc9aefdc15b9fdffe66e4b4
thatsparrow <jbenjamin001@gmail.com>
2020-01-02 13:03:37 +0530

    rm links

--- assets/js/script.js
+++ assets/js/script.js
@@ -97,7 +97,7 @@ function reloadContent() {
 function compileContent() {
      var a = "hc.liamnotorp/worrapstaht".split("").reverse().join("").replace("/", "@");
      var b = {
-        INFO: ['<a class="item" title="?" href="about">About</a>','<a class="item" href="blog">Blog</a>','<a class="item" href="contact">Contact</a>','<a class="item" href="INFO/resume/">Résumé</a>'],
+        INFO: ['<a class="item" title="?" href="about">About</a>','<a class="item" href="blog">Blog</a>','<a class="item" href="contact">Contact</a>','<a class="item" href="resume/">Résumé</a>'],
                
           
          PORTFOLIO_creative: {
@@ -111,23 +111,23 @@ function compileContent() {
          },
 
          PORTFOLIO_software: {
-             16: '<a title="A minimal notepad for heavy deadlines" href="software/notepad-master">Notepad</a> » 💻 : • HTML5 : • CSS3 : • JS',
-	         17: '<a title="For simulating a human typing on-screen" href="FOSS_PROJECTS/type-ai-master">TypeUX</a> » AI : • JavaScript : • CoffeeScript',
-             18: '<a title="A few NLP chatbot implementations written in Python" href="FOSS_PROJECTS/pulse-ai-master">Pulse Chatbot(s)</a> » AI : • Python : • NLTK',
-             19: '<a title="A static website for a digital agency" href="https://www.behance.net/gallery/71725979/OSIRIS">www.osiris.agency</a> » 📱 💻 : • Jekyll : • Semantic UI',
-             20: '<a title="AI: Camera Object Detection" href="compiling">Camera Objection Detection</a> » AI : • Python : • ImageAI : • Keras',
+             16: '<a title="A minimal notepad for heavy deadlines" href="">Notepad</a> » 💻 : • HTML5 : • CSS3 : • JS',
+	         17: '<a title="For simulating a human typing on-screen" href="">TypeUX</a> » AI : • JavaScript : • CoffeeScript',
+             18: '<a title="A few NLP chatbot implementations written in Python" href="">Pulse Chatbots</a> » AI : • Python : • NLTK',
+             19: '<a title="A static website for a digital agency" href="">www.osiris.agency</a> » 📱 💻 : • Jekyll : • Semantic UI',
+             20: '<a title="AI: Camera Object Detection" href="">Camera Objection Detection</a> » AI : • Python : • ImageAI : • Keras',
              21: '<a title="A collaborative code compiler (Python, Java, C, C#, C++, Haskell, Perl, Ruby) built on Heroku and Firebase" href="apps/compile">Compile(r)</a> » 💻 : • Node.js : • Firebase : • Heroku',
-             22: '<a title="A collection of CSS3 effects" href="FOSS_PROJECTS/float-master">FloatUX</a> » 😊 : • JavaScript',
-             23: '<a title="A static website for an apparel boutique" href="https://www.behance.net/gallery/72582967/353-Clothing-Co">www.353clothing.com</a> » 📱 💻 : • Bootstrap : • Slack',
+             22: '<a title="A collection of CSS3 effects" href="">FloatUX</a> » 😊 : • JavaScript',
+             23: '<a title="A static website for an apparel boutique" href="https://www.behance.net/gallery/72582967/353-Clothing-Co">353 Clothing Co.</a> » 📱 💻 : • Bootstrap : • Slack',
              24: '<a title="An Android app that finds petrol stations and splits costs" href="https://github.com/thatsparrow/thatsparrow.github.io/tree/master/FOSS_PROJECTS/insight-master">Insight</a> » 📱 : • Java : • Google Maps API',
-             25: '<a title="A series of number theory problems and their solutions" href="https://github.com/thatsparrow/euler">Project Euler Solutions</a> » >_ : • C : • C++ : • Java : • Python + 3 more',
-             26: '<a title="Technical documentation on the UNIX operating system" href="apps/unix">UNIX101</a> » 📚 : • HTML5 : • LaTeX',
+             25: '<a title="A series of number theory problems and their solutions" href="https://github.com/thatsparrow/euler">Project Euler</a> » >_ : • C : • C++ : • Python',
+             26: '<a title="Technical documentation on the UNIX operating system" href="">UNIX 101</a> » 📚 : • HTML5 : • LaTeX',
              27: '<a title="A few shell scripts to automate your life" href="https://github.com/thatsparrow/scripts">My Scripts</a> » >_ : • Bash : • Fish : • Python ',
-             28: '<a title="A two-body simulation written in JavaScript" href="apps/kepler">Kepler Orbits</a> » 📈 : • JavaScript',
+             28: '<a title="A two-body simulation written in JavaScript" href="">Kepler Orbits</a> » 📈 : • JavaScript',
              29: '<a title="A simulation of wind-driven ocean waves in 2D" href="">2D Ray Tracing</a> » 📈 : • Fortran',
              30: '<a title="An offline html editor" href="">HTML Editor</a> » 📈 : • JavaScript  ',
              31: '<a title="A background music player" href="">Background Music Player</a> » 📈 : • JavaScript',
-             32: '<a title="A calculator written in JavaScript" href="FOSS_PROJECTS/calculator-master">CalculatorJS</a> » 📈 : • JavaScript',
+             32: '<a title="A calculator written in JavaScript" href="">CalculatorJS</a> » 📈 : • JavaScript',
              33: '<a title="A touch-typing interface for improving WPM" href="">Keyboard Warrior</a> » 📱 💻 : • HTML5 : • CSS3 : • JS',
              34: '<a title="A modified KANBAN board" href="">Notice Board UI</a> » 📱 💻 : • HTML5 : • CSS3 : • JS (React.js)',
              35: '<a title="My first Python game!" href="">Bong Hit</a> » 💻 : • Python : • PyGame ',
